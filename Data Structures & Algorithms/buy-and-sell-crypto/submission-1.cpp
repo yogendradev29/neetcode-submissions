cf419390971ccdaf277090d71a1139ef61b655ee
yogendradev29 <yogendra.dev29@gmail.com>
2026-06-07 18:45:18 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans=0;
+        int min_price=INT_MAX;
+        for(int i=0;i<prices.size();i++){
+            if(prices[i]<min_price){
+                min_price=prices[i];
+            }
+            int diff=prices[i]-min_price;
+            ans=max(ans,diff);
+        }
+        return ans;
+
+    }
+};
