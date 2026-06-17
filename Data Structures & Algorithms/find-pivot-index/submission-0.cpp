4e498680c32e5e35ab3d0f1baac68d2ff657c47f
yogendradev29 <yogendra.dev29@gmail.com>
2026-06-17 22:12:54 +0530

    Add: find-pivot-index - submission-0

---
+++ Data Structures & Algorithms/find-pivot-index/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+        }
+        int lsum=0;
+        for(int i=0;i<nums.size();i++){
+            int rsum=sum-lsum-nums[i];
+            
+            
+            if(rsum==lsum){
+                return i;
+            }
+             lsum += nums[i];
+        }
+        return -1;
+    }
+};
