c642a063ed8997d64c40101b3c6e9e6ed79dd602
yogendradev29 <yogendra.dev29@gmail.com>
2026-07-17 21:49:00 +0530

    Add: delete-leaves-with-a-given-value - submission-0

---
+++ Data Structures & Algorithms/delete-leaves-with-a-given-value/submission-0.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+TreeNode* helper(TreeNode* root,int target){
+    if(root==nullptr){
+        return nullptr;
+    }
+    root->left=helper(root->left,target);
+    root->right=helper(root->right,target);
+    if(root->val==target && root->left==nullptr && root->right==nullptr){
+        delete root;
+        return nullptr;
+    }
+    return root;
+}
+  
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+         return helper(root,target);
+    }
+};
