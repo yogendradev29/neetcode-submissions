96158ec584fd7dde8c3e54e5120410eccf12f282
yogendradev29 <yogendra.dev29@gmail.com>
2026-07-23 18:31:29 +0530

    Add: ransom-note - submission-0

---
+++ Data Structures & Algorithms/ransom-note/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int>freq;
+        for(char ch:magazine){
+            freq[ch]++;
+        }
+        for(char ch:ransomNote){
+            freq[ch]--;
+            if(freq[ch]<0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
